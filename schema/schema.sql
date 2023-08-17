d2cb0110547c7af24a7286d90015305c94c9de31
Nathalie Redick <nathalieredick@gmail.com>
2023-08-17 16:20:29 -0400

    fix syntax in schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,25 +2,21 @@ CREATE DATABASE team11;
 
 USE team11;
 
-CREATE TABLE tickers
-(
-    tikcer_id varchar(10)
-    PRIMARY KEY(ticker_id)
-)
+CREATE TABLE tickers (
+    ticker_id varchar(10) PRIMARY KEY
+);
 
--- CREATE TABLE transactions
--- (
---     tran_type varchar(4)
---     amount decimal
---     tran_time
---     ticker_id varchar(10)
+-- CREATE TABLE transactions (
+--     tran_type varchar(4),
+--     amount decimal,
+--     tran_time timestamp,
+--     ticker_id varchar(10),
 --     FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
--- )
+-- );
 
-CREATE TABLE tickersData
-(
-    ticker_id varchar(10)
-    price decimal
-    timestamp timestamp
+CREATE TABLE tickersData (
+    ticker_id varchar(10),
+    price decimal,
+    timestamp timestamp,
     FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
-)
+);
