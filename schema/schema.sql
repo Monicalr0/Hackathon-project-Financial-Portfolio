fe236a3fd7b32dfc7af125ba43fc69301632066f
Minyi <45950996+Monicalr0@users.noreply.github.com>
2023-08-18 16:54:38 -0400

    updated schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -6,17 +6,24 @@ CREATE TABLE tickers (
     ticker_id varchar(10) PRIMARY KEY
 );
 
--- CREATE TABLE transactions (
---     tran_type varchar(4),
---     amount decimal,
---     tran_time timestamp,
---     ticker_id varchar(10),
---     FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
--- );
-
 CREATE TABLE tickersData (
     ticker_id varchar(10),
     price decimal,
-    timestamp timestamp,
+    date timestamp,
+    FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
+);
+
+CREATE TABLE portfolio (
+    ticker_id varchar(10),
+    num_share int,
+    buy_in_price decimal,
+    totalReturn decimal
+    FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
+);
+
+CREATE TABLE tickersReturn (
+    ticker_id varchar(10),
+    meanReturn decimal,
+    date timestamp
     FOREIGN KEY(ticker_id) REFERENCES tickers(ticker_id)
 );
